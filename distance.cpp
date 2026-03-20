7a6976f6d72ef40f37bb2fae37c6e06d4c689f41
wjdaude <jmlee7733@gmail.com>
2026-03-20 22:53:10 +0900

    add 2D euclidean distance calculator

---
+++ distance.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    double x1,y1;
+    double x2,y2;
+cin >> x1 >> y1 >> x2 >> y2;
+cout << fixed << setprecision(1) << sqrt(pow(x1-x2,2)+pow(y1-y2,2));
+
+}
